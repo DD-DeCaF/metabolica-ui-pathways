62208ad2e7e980c8da8f7b46ef7a59c550e481cb
Alba Lopez Benito <alblop@biosustain.dtu.dk>
2018-03-05 15:23:57 +0100

    removed lodash dependency from ws_pathways.ts

--- src/pathways/services/ws_pathways.ts
+++ src/pathways/services/ws_pathways.ts
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-import * as _ from 'lodash';
 import { PathwaysWSProvider } from '../providers/pathwaysws.provider';
 
 import 'angular-toastr';
