301d011846b3db948cba4cdb1225c50db290fe7c
Svetlana <galkina.s.a@gmail.com>
2018-03-12 14:41:33 +0100

    fix: blank lines

--- src/pathways/providers/pathwaysws.provider.ts
+++ src/pathways/providers/pathwaysws.provider.ts
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-
 export class PathwaysWSProvider {
   private host = 'wss://api-staging.dd-decaf.eu/pathways';
   private prefix = '/ws';
