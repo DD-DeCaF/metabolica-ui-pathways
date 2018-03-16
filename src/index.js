34a449c5dedecb6e671aa35753ed84dcb02dab90
Svetlana <galkina.s.a@gmail.com>
2018-03-16 17:15:14 +0100

    style: one blank line after the license

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 import angular from 'angular';
 import {DevAppModule} from 'metabolica';
 import {PathwaysModule} from './pathways/pathways.module';
